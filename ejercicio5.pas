be157ae1d29a26883f9fe7625c5d012c7832f2cf
maichcornejo <108956214+maichcornejo@users.noreply.github.com>
2022-08-21 21:42:05 -0300

    Add files via upload

---
+++ ejercicio5.pas
@@ -0,0 +1,16 @@
+program ejercicio5;
+    type
+        tNombres= (Maia, Lucas, Xanax, Ginebra, walter, euge, carlos); 
+    var 
+        nombre: string;
+        var1:tNombres;
+    procedure EscribirNombre(nombre: tnombres);   
+
+    begin
+        read(nombre);
+        if (nombre=var1) then
+        begin
+        writeln(nombre);
+        end
+    end.
+    
